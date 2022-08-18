7a58dda96ed593d822092616dba0375d5c86400a
22suraj <surajjadhav525k@gmail.com>
2022-08-18 12:14:59 +0530

    Updatef

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {},
+    colors: {
+      "custom-orange": "#6C63FF",
+      "custome-ligorg": "#C9C7FF",
+    },
+  },
+  plugins: [],
+};
